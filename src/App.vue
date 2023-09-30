7b276142d157498a370b2b3de3570edbe56033c8
hamza99113 <alihamza99113@gmail.com>
2023-09-30 11:11:05 +0500

    Added StoredResources Component

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- <ul>
+    <LearningResource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul> -->
+  <div>
+    <StoredResources :resources="storedResources"/>
+  </div>
+  
+</template>
+
+<script>
+// import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: {
+    // LearningResource,
+    StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn more about google.',
+          link: 'https://google.org/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
